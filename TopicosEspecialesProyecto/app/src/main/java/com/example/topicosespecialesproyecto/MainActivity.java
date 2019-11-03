2f6803e1e1459bf81fef09ed711903ea5dca33c0
AngelMiguelPJ <miguel.pozo@epn.edu.ec>
2019-11-03 16:51:02 -0500

    Creacion de proyecto, interfaz del login

---
+++ TopicosEspecialesProyecto/app/src/main/java/com/example/topicosespecialesproyecto/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.topicosespecialesproyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
