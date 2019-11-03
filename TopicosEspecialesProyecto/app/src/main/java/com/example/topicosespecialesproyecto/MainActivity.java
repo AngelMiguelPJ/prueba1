a314af809826300d8bbf1e200d7ecb7aedea46a7
AngelMiguelPJ <miguel.pozo@epn.edu.ec>
2019-11-03 17:22:55 -0500

    Interfaz de registro y funcionalidad de intercambio de ventanas

--- TopicosEspecialesProyecto/app/src/main/java/com/example/topicosespecialesproyecto/MainActivity.java
+++ TopicosEspecialesProyecto/app/src/main/java/com/example/topicosespecialesproyecto/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.topicosespecialesproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Variables
+    Button btn1;
+    Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // busqueda de las variables
+        // button de iniciar sesion
+        btn1 = (Button) findViewById(R.id.button);
+        btn1.setOnClickListener(this);
+        // button de ir a la pagina de registro
+        btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btn1){
+        }
+        else if (v == btn2){
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        }
     }
 }
